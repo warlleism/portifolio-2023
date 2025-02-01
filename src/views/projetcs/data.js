91c00dfa0decff92e3d445ae14adb9400d2e222b
Warllei <warllei.s.martins@hotmail.com>
2025-02-01 17:11:48 -0300

    mais projetos

--- src/views/projetcs/data.js
+++ src/views/projetcs/data.js
@@ -24,16 +24,32 @@ import pj23 from "../../assets/img/projects/pj23.png";
 import pj24 from "../../assets/img/projects/pj24.png";
 import pj25 from "../../assets/img/projects/pj25.png";
 import pj26 from "../../assets/img/projects/pj26.png";
+import pj27 from "../../assets/img/projects/pj27.png";
 
 export const Data = [
   {
     id: 1,
+    name: "Node Nest Fast Food Api",
+    side: "Back-end",
+    git: "https://github.com/warlleism/free-fast-food-api-ecommerce",
+    skills: [
+      { skill: "Node" },
+      { skill: "Nest" },
+      { skill: "TypeScript" },
+      { skill: "jsonwebtoken" },
+      { skill: "Swagger" },
+    ],
+    img: pj27,
+  },
+  {
+    id: 2,
     name: "Web Vue Nuxt Ecommerce ",
     side: "Front-end",
     git: "https://github.com/warlleism/vue-nuxt-ecommerce",
     skills: [
       { skill: "Vue" },
       { skill: "Nuxt" },
+      { skill: "TypeScript" },
       { skill: "Tailwind" },
       { skill: "Fetch" },
       { skill: "Pinia" },
@@ -41,7 +57,7 @@ export const Data = [
     img: pj26,
   },
   {
-    id: 2,
+    id: 3,
     name: "Ecommerce Mobile App (IOS & Android)",
     side: "Front-end",
     git: "https://github.com/warlleism/react-native-v0.76.5-build-project",
@@ -60,7 +76,7 @@ export const Data = [
     img: pj25,
   },
   {
-    id: 3,
+    id: 4,
     name: "Gerênciamanto médico Web App",
     side: "Full-stack",
     git: "https://github.com/warlleism/fullstack-system-of-medical-consultations-in-angular-nest-prisma-postgresql",
@@ -77,7 +93,7 @@ export const Data = [
     img: pj22,
   },
   {
-    id: 4,
+    id: 5,
     name: "E-Commerce Web App",
     side: "Full-stack",
     git: "https://github.com/warlleism/node-next-full-stack-ecommerce",
@@ -95,7 +111,7 @@ export const Data = [
     img: pj17,
   },
   {
-    id: 5,
+    id: 6,
     name: "Chat Online Web App",
     side: "Full-stack",
     git: "https://github.com/warlleism/chat-socket-io-node-typescript",
@@ -111,11 +127,10 @@ export const Data = [
     img: pj23,
   },
   {
-    id: 6,
+    id: 7,
     name: "Ecommerce Back-end",
     side: "Back-end",
     git: "https://github.com/warlleism/backend-ecommerce-node-api-typeorm-postgresql",
-    side: "Back-end",
     skills: [
       { skill: "Node.Js" },
       { skill: "TypeOrm" },
@@ -126,7 +141,7 @@ export const Data = [
     img: pj18,
   },
   {
-    id: 7,
+    id: 8,
     name: "Next Auth Web App",
     side: "Front-end",
     git: "https://github.com/warlleism/next-14-auth-google-github",
@@ -142,7 +157,7 @@ export const Data = [
     img: pj13,
   },
   {
-    id: 8,
+    id: 9,
     name: "News Website",
     side: "Front-end",
     git: "https://github.com/warlleism/angular-app-news-api-user-authentication",
@@ -154,7 +169,7 @@ export const Data = [
     img: pj21,
   },
   {
-    id: 9,
+    id: 10,
     name: "Djando python api",
     side: "Back-end",
     git: "https://github.com/warlleism/python-user-authentication-cryptography-tokenJWT",
@@ -169,7 +184,7 @@ export const Data = [
     img: pj20,
   },
   {
-    id: 10,
+    id: 11,
     name: "Landing Page Marmoraria",
     side: "Front-end",
     git: "https://github.com/warlleism/mf-website",
@@ -182,7 +197,7 @@ export const Data = [
     img: pj14,
   },
   {
-    id: 11,
+    id: 12,
     name: "Website sobre a natureza",
     side: "Full-stack",
     link: "https://www.behance.net/gallery/170124931/Web-Design",
@@ -200,7 +215,7 @@ export const Data = [
     img: pj10,
   },
   {
-    id: 12,
+    id: 13,
     name: "Restaurante Mobile App",
     side: "Front-end",
     link: "https://www.behance.net/gallery/170008105/Design-App-Mobile",
@@ -215,7 +230,7 @@ export const Data = [
     img: pj6,
   },
   {
-    id: 13,
+    id: 14,
     name: "Filtro de buscar Smart fit",
     side: "Front-end",
     git: "https://github.com/warlleism/front-end-smartfit-challenge",
@@ -230,7 +245,7 @@ export const Data = [
     img: pj15,
   },
   {
-    id: 14,
+    id: 15,
     name: "Restaurante Landing Page",
     side: "Web Design",
     link: "https://www.behance.net/gallery/171905571/Web-Design",
@@ -238,7 +253,7 @@ export const Data = [
     img: pj5,
   },
   {
-    id: 15,
+    id: 16,
     name: "Plataforma de cursos mobile",
     side: "Front-end",
     git: "https://github.com/warlleism/warlleism-react-native-course-platform",
@@ -254,7 +269,7 @@ export const Data = [
     img: pj16,
   },
   {
-    id: 16,
+    id: 17,
     name: "Portifólio web/ui Design",
     link: "https://portifoliowarllei.netlify.app/",
     git: "https://github.com/warlleism/portifolio-2023",
@@ -273,16 +288,15 @@ export const Data = [
     img: pj1,
   },
   {
-    id: 17,
+    id: 18,
     name: "Landing Page spotify",
     side: "Web Design",
     link: "https://www.behance.net/gallery/170008613/Web-Design",
-    side: "Web Design",
     skills: [{ skill: "Figma" }, { skill: "Web Design" }],
     img: pj19,
   },
   {
-    id: 18,
+    id: 19,
     name: "catálogo & Reserva de restaurante",
     link: "https://www.behance.net/gallery/174203861/Mobile-Design",
     git: "https://github.com/warlleism/restaurant-reservation-registration-mobile-app",
@@ -299,7 +313,7 @@ export const Data = [
     img: pj2,
   },
   {
-    id: 19,
+    id: 20,
     name: "Landing Page Cultura negra",
     side: "Web Design",
     link: "https://www.behance.net/gallery/172466731/Web-Design",
@@ -307,7 +321,7 @@ export const Data = [
     img: pj3,
   },
   {
-    id: 20,
+    id: 21,
     name: "Web App",
     side: "Front-end",
     link: "https://www.behance.net/gallery/170008457/Web-Design",
@@ -321,7 +335,7 @@ export const Data = [
     img: pj4,
   },
   {
-    id: 21,
+    id: 22,
     name: "Landing Page de restaurante",
     side: "Web Design",
     link: "https://www.behance.net/gallery/171520757/Web-Design",
@@ -329,7 +343,7 @@ export const Data = [
     img: pj7,
   },
   {
-    id: 22,
+    id: 23,
     name: "Starbucks Mobile design",
     side: "Web Design",
     link: "https://www.behance.net/gallery/170950771/Mobile-Design",
@@ -337,7 +351,7 @@ export const Data = [
     img: pj8,
   },
   {
-    id: 23,
+    id: 24,
     name: "Landing Page da nike",
     side: "Web Design",
     link: "https://www.behance.net/gallery/170872399/Web-Design",
@@ -345,7 +359,7 @@ export const Data = [
     img: pj9,
   },
   {
-    id: 24,
+    id: 25,
     name: "Landing Page da coca-cola",
     side: "Web Design",
     link: "https://www.behance.net/gallery/170008221/Landing-Page-Design",
@@ -353,7 +367,7 @@ export const Data = [
     img: pj11,
   },
   {
-    id: 25,
+    id: 26,
     name: "Landing Page da dentista",
     side: "Web Design",
     link: "https://www.behance.net/gallery/176480859/Web-Design",
@@ -361,7 +375,7 @@ export const Data = [
     img: pj12,
   },
   {
-    id: 26,
+    id: 27,
     name: "Arte Web App",
     side: "Front-end",
     git: "https://github.com/warlleism/return-react-vite-website",
